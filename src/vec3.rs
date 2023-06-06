9731ea464945a9ebe70540ec98c95486c6d0ed6f
MakisChristou <makis_christou@protonmail.com>
2023-06-06 21:57:48 +0300

    Dot and Cross product

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,19 +1,19 @@
 use std::ops;
 
 #[derive(Debug, PartialEq)]
-pub struct vec3 {
+pub struct Vec3 {
     pub x: f64,
     pub y: f64,
-    pub z: f64,   
+    pub z: f64,
 }
 
-impl vec3 {
-    pub fn new(x: f64, y: f64, z: f64) -> Self{
-        vec3{x,y,z}
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3 { x, y, z }
     }
 
-    pub fn from_one(a: f64) -> Self{
-        vec3{x: a, y: a, z: a}
+    pub fn from_one(a: f64) -> Self {
+        Vec3 { x: a, y: a, z: a }
     }
 
     pub fn length(&self) -> f64 {
@@ -23,112 +23,193 @@ impl vec3 {
     pub fn length_squared(&self) -> f64 {
         self.x * self.x + self.y * self.y + self.z * self.z
     }
-}
 
-impl ops::Add<vec3> for vec3 {
-    type Output = vec3;
-    fn add(self, _rhs: vec3) -> vec3 {
-        vec3::new(self.x + _rhs.x, self.y + _rhs.y, self.z + _rhs.z) 
+    pub fn dot(&self, u: &Vec3) -> f64 {
+        self.x * u.x + self.y * u.y + self.z * u.z
+    }
+
+    pub fn cross(&self, u: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * u.z - self.z * u.y,
+            y: self.z * u.x - self.x * u.z,
+            z: self.x * u.y - self.y * u.x,
+        }
     }
 }
 
-impl ops::Sub<vec3> for vec3 {
-    type Output = vec3;
-    fn sub(self, _rhs: vec3) -> vec3 {
-        vec3::new(self.x - _rhs.x, self.y - _rhs.y, self.z - _rhs.z) 
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(self.x + _rhs.x, self.y + _rhs.y, self.z + _rhs.z)
     }
 }
 
-impl ops::Mul<vec3> for vec3 {
-    type Output = vec3;
-
-    fn mul(self, _rhs: vec3) -> vec3{
-        vec3::new(self.x * _rhs.x, self.y * _rhs.y, self.z * _rhs.z) 
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(self.x - _rhs.x, self.y - _rhs.y, self.z - _rhs.z)
     }
 }
 
-impl ops::Mul<f64> for vec3 {
-    type Output = vec3;
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
 
-    fn mul(self, _rhs: f64) -> vec3{
-        vec3::new(self.x * _rhs, self.y * _rhs, self.z * _rhs) 
+    fn mul(self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(self.x * _rhs.x, self.y * _rhs.y, self.z * _rhs.z)
     }
 }
 
-impl ops::Div<f64> for vec3 {
-    type Output = vec3;
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
 
-    fn div(self, _rhs: f64) -> vec3{
-        vec3::new(self.x / _rhs, self.y / _rhs, self.z / _rhs) 
+    fn mul(self, _rhs: f64) -> Vec3 {
+        Vec3::new(self.x * _rhs, self.y * _rhs, self.z * _rhs)
     }
 }
 
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, _rhs: f64) -> Vec3 {
+        Vec3::new(self.x / _rhs, self.y / _rhs, self.z / _rhs)
+    }
+}
 
 #[cfg(test)]
-mod tests{
-    use crate::vec3::vec3;
-
+mod tests {
+    use crate::vec3::Vec3;
 
     #[test]
     fn should_instantiate_vector() {
-        let v = vec3::new(1.0, 2.0, 3.0);
-        assert_eq!(v, vec3{x: 1.0, y: 2.0, z: 3.0});
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(
+            v,
+            Vec3 {
+                x: 1.0,
+                y: 2.0,
+                z: 3.0
+            }
+        );
     }
 
     #[test]
     fn should_instantiate_from_one() {
-        let v = vec3::from_one(1.0);
-        assert_eq!(v, vec3{x: 1.0, y: 1.0, z: 1.0});
+        let v = Vec3::from_one(1.0);
+        assert_eq!(
+            v,
+            Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0
+            }
+        );
     }
 
     #[test]
-    fn should_have_correct_length_squared(){
-        let v = vec3::new(1.0, 1.0, 5.0);
+    fn should_have_correct_length_squared() {
+        let v = Vec3::new(1.0, 1.0, 5.0);
         assert_eq!(v.length_squared(), 27.0);
     }
 
     #[test]
-    fn should_have_correct_length(){
-        let v = vec3::new(3.0, 2.0, 5.0);
+    fn should_have_correct_length() {
+        let v = Vec3::new(3.0, 2.0, 5.0);
         assert_eq!(v.length(), f64::sqrt(38.0));
     }
 
     #[test]
-    fn should_add_correctly(){
-        let v1 = vec3::new(3.0, 2.0, 5.0);
-        let v2 = vec3::new(4.0, 6.0, 5.0);
+    fn should_add_correctly() {
+        let v1 = Vec3::new(3.0, 2.0, 5.0);
+        let v2 = Vec3::new(4.0, 6.0, 5.0);
         let v3 = v1 + v2;
-        assert_eq!(v3, vec3{x: 7.0, y: 8.0, z: 10.0});
+        assert_eq!(
+            v3,
+            Vec3 {
+                x: 7.0,
+                y: 8.0,
+                z: 10.0
+            }
+        );
     }
 
     #[test]
-    fn should_sub_correctly(){
-        let v1 = vec3::new(3.0, 2.0, 5.0);
-        let v2 = vec3::new(4.0, 6.0, 5.0);
+    fn should_sub_correctly() {
+        let v1 = Vec3::new(3.0, 2.0, 5.0);
+        let v2 = Vec3::new(4.0, 6.0, 5.0);
         let v3 = v1 - v2;
-        assert_eq!(v3, vec3{x: -1.0, y: -4.0, z: 0.0});
+        assert_eq!(
+            v3,
+            Vec3 {
+                x: -1.0,
+                y: -4.0,
+                z: 0.0
+            }
+        );
     }
 
     #[test]
-    fn should_mul_vec_correctly(){
-        let v1 = vec3::new(3.0, 2.0, 5.0);
-        let v2 = vec3::new(4.0, 6.0, 5.0);
+    fn should_mul_vec_correctly() {
+        let v1 = Vec3::new(3.0, 2.0, 5.0);
+        let v2 = Vec3::new(4.0, 6.0, 5.0);
         let v3 = v1 * v2;
-        assert_eq!(v3, vec3{x: 12.0, y: 12.0, z: 25.0});
+        assert_eq!(
+            v3,
+            Vec3 {
+                x: 12.0,
+                y: 12.0,
+                z: 25.0
+            }
+        );
     }
 
     #[test]
-    fn should_mul_correctly(){
-        let v1 = vec3::new(3.0, 2.0, 5.0);
+    fn should_mul_correctly() {
+        let v1 = Vec3::new(3.0, 2.0, 5.0);
         let v3 = v1 * 10.0;
-        assert_eq!(v3, vec3{x: 30.0, y: 20.0, z: 50.0});
+        assert_eq!(
+            v3,
+            Vec3 {
+                x: 30.0,
+                y: 20.0,
+                z: 50.0
+            }
+        );
     }
 
     #[test]
-    fn should_div_correctly(){
-        let v1 = vec3::new(30.0, 20.0, 50.0);
+    fn should_div_correctly() {
+        let v1 = Vec3::new(30.0, 20.0, 50.0);
         let v3 = v1 / 10.0;
-        assert_eq!(v3, vec3{x: 3.0, y: 2.0, z: 5.0});
+        assert_eq!(
+            v3,
+            Vec3 {
+                x: 3.0,
+                y: 2.0,
+                z: 5.0
+            }
+        );
     }
 
+    #[test]
+    fn should_dot_correctly() {
+        let v1 = Vec3::new(3.0, 2.0, 5.0);
+        let v2 = Vec3::new(4.0, 6.0, 5.0);
+        let v3 = v1.dot(&v2);
+        assert_eq!(v3, 49.0);
+    }
+
+    #[test]
+    fn should_cross_correctly() {
+        let v1 = Vec3::new(3.0, 2.0, 5.0);
+        let v2 = Vec3::new(4.0, 6.0, 5.0);
+        let v3 = v1.cross(&v2);
+        assert_eq!(
+            v3,
+            Vec3 {
+                x: -20.0,
+                y: 5.0,
+                z: 10.0
+            }
+        );
+    }
 }
