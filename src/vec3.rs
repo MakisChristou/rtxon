56de89adae929dbaac55c7fa5085e5371ecda1fe
MakisChristou <makis_christou@protonmail.com>
2023-06-06 21:34:53 +0300

    Mul vec3 and mul f64

--- src/vec3.rs
+++ src/vec3.rs
@@ -39,6 +39,22 @@ impl ops::Sub<vec3> for vec3 {
     }
 }
 
+impl ops::Mul<vec3> for vec3 {
+    type Output = vec3;
+
+    fn mul(self, _rhs: vec3) -> vec3{
+        vec3::new(self.x * _rhs.x, self.y * _rhs.y, self.z * _rhs.z) 
+    }
+}
+
+impl ops::Mul<f64> for vec3 {
+    type Output = vec3;
+
+    fn mul(self, _rhs: f64) -> vec3{
+        vec3::new(self.x * _rhs, self.y * _rhs, self.z * _rhs) 
+    }
+}
+
 
 #[cfg(test)]
 mod tests{
@@ -49,6 +65,12 @@ mod tests{
     fn should_instantiate_vector() {
         let v = vec3::new(1.0, 2.0, 3.0);
         assert_eq!(v, vec3{x: 1.0, y: 2.0, z: 3.0});
+    }
+
+    #[test]
+    fn should_instantiate_from_one() {
+        let v = vec3::from_one(1.0);
+        assert_eq!(v, vec3{x: 1.0, y: 1.0, z: 1.0});
     }
 
     #[test]
@@ -77,6 +99,21 @@ mod tests{
         let v2 = vec3::new(4.0, 6.0, 5.0);
         let v3 = v1 - v2;
         assert_eq!(v3, vec3{x: -1.0, y: -4.0, z: 0.0});
+    }
+
+    #[test]
+    fn should_mul_vec_correctly(){
+        let v1 = vec3::new(3.0, 2.0, 5.0);
+        let v2 = vec3::new(4.0, 6.0, 5.0);
+        let v3 = v1 * v2;
+        assert_eq!(v3, vec3{x: 12.0, y: 12.0, z: 25.0});
+    }
+
+    #[test]
+    fn should_mul_correctly(){
+        let v1 = vec3::new(3.0, 2.0, 5.0);
+        let v3 = v1 * 10.0;
+        assert_eq!(v3, vec3{x: 30.0, y: 20.0, z: 50.0});
     }
 
 }
