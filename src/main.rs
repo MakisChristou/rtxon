27ec9a75710ea0cbf8d17bda68cca0d241c2b463
MakisChristou <makis_christou@protonmail.com>
2023-06-07 20:21:26 +0300

    Sending rays into the scene

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,43 @@ mod ray;
 mod utils;
 mod vec3;
 
+use ray::Ray;
+use vec3::Vec3;
+
 use crate::utils::color::Color;
 use crate::utils::write_color;
 
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = Vec3::unit_vector(&r.direction());
+    let t = 0.5 * unit_direction.y + 1.0;
+    return Color {
+        r: 1.0,
+        g: 1.0,
+        b: 1.0,
+    } * (1.0 - t)
+        + Color {
+            r: 0.5,
+            g: 0.7,
+            b: 1.0,
+        } * t;
+}
+
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width: usize = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as usize;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
     println!("P3\n{} {}\n255", image_width, image_height);
@@ -16,13 +47,23 @@ fn main() {
         eprint!("\rScanlines remaining: {}", j);
 
         for i in 0..image_width {
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+
+            let r = Ray::new(
+                origin,
+                lower_left_corner + horizontal * u + vertical * v - origin,
+            );
+
+            let pixel_color = ray_color(&r);
+
             let c = Color {
                 r: (i as f64) / (image_width as f64 - 1.0),
                 g: (j as f64) / (image_height as f64 - 1.0),
                 b: 0.25,
             };
 
-            write_color(c);
+            write_color(pixel_color);
         }
     }
 }
--- src/utils/color.rs
+++ src/utils/color.rs
@@ -1,5 +1,29 @@
+use std::ops;
+
 pub struct Color {
     pub r: f64,
     pub g: f64,
     pub b: f64,
+}
+
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Self {
+        Color { r, g, b }
+    }
+}
+
+impl ops::Mul<f64> for Color {
+    type Output = Color;
+
+    fn mul(self, _rhs: f64) -> Color {
+        Color::new(self.r * _rhs, self.g * _rhs, self.b * _rhs)
+    }
+}
+
+impl ops::Add<Color> for Color {
+    type Output = Color;
+
+    fn add(self, _rhs: Color) -> Color {
+        Color::new(self.r + _rhs.r, self.g + _rhs.g, self.b + _rhs.b)
+    }
 }
