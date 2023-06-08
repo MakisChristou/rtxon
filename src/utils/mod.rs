867d3e015883375daeb50e2094aa8dc3a35ce975
MakisChristou <makis_christou@protonmail.com>
2023-06-08 22:06:40 +0300

    The rtweekend.h common header equivalent

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,9 +2,18 @@ pub mod color;
 
 use crate::utils::color::Color;
 
+// Static variables
+static infinity: f64 = std::f64::INFINITY;
+static pi: f64 = std::f64::consts::PI;
+
 pub fn write_color(c: Color) {
     let ir = (255.999 * c.r) as i32;
     let ig = (255.999 * c.g) as i32;
     let ib = (255.999 * c.b) as i32;
     println!("{} {} {}", ir, ig, ib);
 }
+
+pub fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * pi / 180.0
+}
+
