6db9f17aea3044fb23f43e611405327a38fbca22
MakisChristou <makis_christou@protonmail.com>
2023-06-09 22:19:32 +0300

    Change helper functions to Option<T>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -13,9 +13,9 @@ pub fn write_color(c: Color, samples_per_pixel: f64) {
     let mut b = c.b;
 
     let scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = f64::sqrt(scale * r);
+    g = f64::sqrt(scale * g);
+    b = f64::sqrt(scale * b);
 
     let ir = (256.0 * clamp(r, 0.0, 0.999)) as i32;
     let ig = (256.0 * clamp(g, 0.0, 0.999)) as i32;
@@ -28,8 +28,12 @@ pub fn degrees_to_radians(degrees: f64) -> f64 {
 }
 
 // Random Number Utilities
-pub fn random_double() -> f64 {
-    rand::thread_rng().gen_range(0.0..1.0)
+pub fn random_double(range: Option<(f64, f64)>) -> f64 {
+    match range {
+        Some(range) => rand::thread_rng().gen_range(range.0..range.1),
+        None => rand::thread_rng().gen_range(0.0..1.0)
+    }
+    
 }
 
 pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
@@ -47,8 +51,8 @@ mod tests {
     use super::random_double;
 
     #[test]
-    fn should_return_correct_random_numnber() {
-        let r = random_double();
+    fn should_return_correct_default_random_numnber() {
+        let r = random_double(None);
         assert!(r < 1.0 && r > 0.0);
     }
 }
